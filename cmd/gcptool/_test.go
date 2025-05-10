b48a38332e92bccd4e6dd635fdfdcfabba5b8e19
venkyGeek <venkateshpoola1998@gmail.com>
2025-05-11 04:31:58 +0530

    added unit tests

---
+++ cmd/gcptool/_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestCarbonquest(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	rr := httptest.NewRecorder()
+
+	Carbonquest(rr, req)
+
+	result := rr.Body.String()
+	expected := "Hello from Cloud Function!"
+
+	if !strings.Contains(result, expected) {
+		t.Errorf("Expected response to contain %q, but got %q", expected, result)
+	}
+}
