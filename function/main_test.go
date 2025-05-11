8d9c511424f47309de6a091a368572187e14ecc5
venkyGeek <venkateshpoola1998@gmail.com>
2025-05-11 08:33:16 +0530

    Code format

--- function/main_test.go
+++ function/main_test.go
@@ -1,27 +1,27 @@
 package function
 
 import (
-    "net/http/httptest"
-    "regexp"
-    "strings"
-    "testing"
+	"net/http/httptest"
+	"regexp"
+	"strings"
+	"testing"
 )
 
 func TestCarbonquest(t *testing.T) {
-    req := httptest.NewRequest("GET", "/", nil)
-    rr := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+	rr := httptest.NewRecorder()
 
-    Carbonquest(rr, req)
+	Carbonquest(rr, req)
 
-    result := rr.Body.String()
+	result := rr.Body.String()
 
-    // Remove HTML tags
-    re := regexp.MustCompile("<[^>]*>")
-    strippedResult := re.ReplaceAllString(result, "")
+	// Remove HTML tags
+	re := regexp.MustCompile("<[^>]*>")
+	strippedResult := re.ReplaceAllString(result, "")
 
-    expected := "Hello from CarbonQuest Cloud Function!"
+	expected := "Hello from CarbonQuest Cloud Function!"
 
-    if !strings.Contains(strippedResult, expected) {
-        t.Errorf("Expected response to contain %q, but got %q", expected, strippedResult)
-    }
+	if !strings.Contains(strippedResult, expected) {
+		t.Errorf("Expected response to contain %q, but got %q", expected, strippedResult)
+	}
 }
